b6b0642a69f0a040bc35982f318fe3404413134e
Ahmad Raza <pro.se.ahmad.raza.1@gmail.com>
2024-01-27 02:18:10 +0500

    init

---
+++ configs/conform.lua
@@ -0,0 +1,24 @@
+local options = {
+  formatters_by_ft = {
+    html = { "prettier" },
+    css = { "prettier" },
+    javascript = { "prettier" },
+    typescript = { "prettier" },
+    scss = { "prettier" },
+    json = { "prettier" },
+    javascriptreact = { "prettier" }, -- Corrected to javascriptreact
+    typescriptreact = { "prettier" }, -- Corrected to typescriptreact
+    yaml = { "prettier" },
+    markdown = { "prettier" }, -- Corrected to markdown
+    lua = { "stylua" },
+    sh = { "shfmt" },
+    zsh = { "shfmt" },
+  },
+
+  format_on_save = {
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
+}
+
+require("conform").setup(options)
