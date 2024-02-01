5416e077536a716f081038b5bd26323bba54f6b7
Ahmad Raza <pro.se.ahmad.raza.1@gmail.com>
2024-02-02 00:14:57 +0500

    Update plugins.lua

--- plugins.lua
+++ plugins.lua
@@ -57,6 +57,17 @@ local plugins = {
   },
   
   {
+  'Exafunction/codeium.vim',
+    config = function ()
+      -- Change '<C-g>' here to any keycode you like.
+      vim.keymap.set('i', '<C-g>', function () return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
+      vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true, silent = true })
+      vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true, silent = true })
+      vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
+    end
+  },
+  
+  {
      "mg979/vim-visual-multi",
      lazy = false,
   }
